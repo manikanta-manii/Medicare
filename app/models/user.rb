3bf6c2967f28500b81d1fb13f4548a3f3ee1ebca
manikanta-manii <manikantap036@gmail.com>
2024-04-26 00:07:55 +0530

    Add some styles to - [ navabar , sidebar ]

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,14 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_one_attached :avatar
+  has_one :patient,dependent: :destroy,inverse_of: :user
+
   validates :name, presence: true, length: { minimum: 2, maximum: 20 }
-  
   validates :phone_number, presence: true, numericality: { only_integer: true }, format: { with: /\A\d{10}\z/, message: "must be a 10-digit number" }
    
-  has_one :patient,dependent: :destroy,inverse_of: :user
+  
   accepts_nested_attributes_for :patient
-
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   enum role: [:admin, :doctor, :patient]
@@ -18,4 +19,5 @@ class User < ApplicationRecord
   def set_default_role
    self.role ||= :patient
   end
+
 end
