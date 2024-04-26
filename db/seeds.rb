1a93dd99ab3e9627311cc4d75444ac963250b426
manikanta-manii <manikantap036@gmail.com>
2024-04-26 10:15:09 +0530

    Add separate navbar and sidebar for roles - [ Admin , Doctor , Patient ]

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,5 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+User.create(name:"Admin",phone_number:"9999999999",email:"admin@gmail.com",password:"Admin@123");
