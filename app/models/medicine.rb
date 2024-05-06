d9ee2aae943fa39964bf43103d6f2681c29c907d
Manikanta Parameswarappa <mparameswarappa@C02DG6UQMD6M.local>
2024-05-06 18:50:40 +0530

    minor changes

--- app/models/medicine.rb
+++ app/models/medicine.rb
@@ -1,8 +1,10 @@
 class Medicine < ApplicationRecord
     has_many  :cart_items,dependent: :destroy
-    has_many  :order_items,dependent: :destroy
+    
     validates :name, :description, :dosage, :price,  presence: true
     validates :price, numericality: { greater_than: 0 }
-
+     
+    has_many  :order_items,dependent: :destroy
+    has_many :orders , through: :order_items
     has_one_attached :image
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,6 @@
 class Order < ApplicationRecord
   has_one_attachment :prescription
   has_many :order_items
+  has_many :medicines , through: :order_items
   belongs_to :patient
 end
