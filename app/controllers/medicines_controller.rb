3c07d85542078c35ac07d9d5160db5081bfe69b2
manikanta-manii <manikantap036@gmail.com>
2024-05-12 02:24:44 +0530

    Medicine Edit from

--- app/controllers/medicines_controller.rb
+++ app/controllers/medicines_controller.rb
@@ -1,6 +1,6 @@
 class MedicinesController < ApplicationController
-    skip_before_action :verify_authenticity_token, only: [:create ,:destroy]
-    before_action :set_medicine, only: [:destroy]
+    skip_before_action :verify_authenticity_token, only: [:create ,:destroy ,:update]
+    before_action :set_medicine, only: [:destroy , :update]
 
     def index
       @medicines = Medicine.all
@@ -13,6 +13,11 @@ class MedicinesController < ApplicationController
       else
         render partial: "shared/errors",locals:{record:@medicine}
       end 
+    end
+
+    def update
+      @medicine.update(medicine_params)
+      render partial: "admin/manage_medicines/each_medicine",locals:{medicine:@medicine}
     end
 
     def destroy
